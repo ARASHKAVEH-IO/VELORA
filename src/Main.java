ccb066b3fd124d6c72a63f442fc1cb33023bd6dc
Arash Kaveh <arashkaveh2006@gmail.com>
2025-09-21 10:15:47 +0330

    main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!\n");
-        System.out.print("Arash Kaveh!");
+        // Student data
+        String studentName = "Arash Kaveh";
+        int studentID = 7;
+        int studentAge = 20;
+        float studentFee = 75.25f;
+        char studentGrade = 'A';
+
+        // Print variables
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
     }
 }
